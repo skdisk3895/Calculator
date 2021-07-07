b5314406d9f26a6be384bcd5e9d95494f14f91f8
HyeBin <skdisk7368@naver.com>
2021-07-08 04:13:13 +0900

    중위표기식 출력 테스트 확인

--- app/src/main/java/com/example/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -14,8 +14,6 @@ public class MainActivity extends AppCompatActivity {
     private TextView tv_display, tv_result;
     private Button btn_zero, btn_one, btn_two, btn_three, btn_four, btn_five, btn_six, btn_seven, btn_eight, btn_nine, btn_add, btn_sub, btn_mult, btn_div, btn_res, btn_clear;
     private String display = "";
-    private String number = "";
-    private ArrayList<String> total_expression = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,11 +42,6 @@ public class MainActivity extends AppCompatActivity {
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                System.out.println(number);
-                if (number == "") return;
-                total_expression.add(number);
-                total_expression.add("+");
-                number = "";
                 display += "+";
                 tv_display.setText(display);
             }
@@ -57,13 +50,12 @@ public class MainActivity extends AppCompatActivity {
         btn_res.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (number.length() > 0) {
-                   total_expression.add(number);
+                String result = tv_display.getText().toString();
+                char lastCharacter = result.charAt(result.length()-1);
+                // System.out.println(lastCharacter);
+                if (Character.isDigit(lastCharacter)) {
+                    System.out.println(result);
                 }
-                for (String i: total_expression) {
-                    System.out.print(i + " ");
-                }
-                System.out.print("\n");
             }
         });
 
@@ -72,14 +64,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 display = "";
                 tv_display.setText(display);
-                total_expression.clear();
             }
         });
 
         btn_zero.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (display.length() > 2) return;
                 display += "0";
                 tv_display.setText(display);
             }
@@ -89,7 +79,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 display += "1";
-                number += "1";
                 tv_display.setText(display);
             }
         });
@@ -97,7 +86,6 @@ public class MainActivity extends AppCompatActivity {
         btn_two.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (display.length() == 3) return;
                 display += "2";
                 tv_display.setText(display);
             }
@@ -106,7 +94,6 @@ public class MainActivity extends AppCompatActivity {
         btn_three.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (display.length() == 3) return;
                 display += "3";
                 tv_display.setText(display);
             }
@@ -115,7 +102,6 @@ public class MainActivity extends AppCompatActivity {
         btn_four.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (display.length() == 3) return;
                 display += "4";
                 tv_display.setText(display);
             }
@@ -124,7 +110,6 @@ public class MainActivity extends AppCompatActivity {
         btn_five.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (display.length() == 3) return;
                 display += "5";
                 tv_display.setText(display);
             }
@@ -133,7 +118,6 @@ public class MainActivity extends AppCompatActivity {
         btn_six.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (display.length() == 3) return;
                 display += "6";
                 tv_display.setText(display);
             }
@@ -142,7 +126,6 @@ public class MainActivity extends AppCompatActivity {
         btn_seven.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (display.length() == 3) return;
                 display += "7";
                 tv_display.setText(display);
             }
@@ -151,7 +134,6 @@ public class MainActivity extends AppCompatActivity {
         btn_eight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (display.length() == 3) return;
                 display += "8";
                 tv_display.setText(display);
             }
@@ -160,7 +142,6 @@ public class MainActivity extends AppCompatActivity {
         btn_nine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (display.length() == 3) return;
                 display += "9";
                 tv_display.setText(display);
             }
